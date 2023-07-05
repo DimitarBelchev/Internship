ade84765285e889ac3494f52bed85039ce7fd8fe
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2023-07-05 11:20:52 +0300

    update
    update

---
+++ objandred_test/objAndRed.js
@@ -0,0 +1,64 @@
+// class Pizza {
+//   constructor(name, ingredients) {
+//     this.name = name;
+//     this.ingredients = ingredients;
+//   }
+// }
+
+// class PizzaRestaurant {
+//   constructor() {
+//     this.pizzas = [];
+//     this.recognizedIngredients = [
+//       "Tomato",
+//       "Mozzarella",
+//       "Basil",
+//       "Pepperoni",
+//       "Pineapple",
+//       "Ham",
+//       "Onions",
+//     ];
+//   }
+
+//   preparePizza(name, ingredients) {
+//     if (this.hasPizza(name)) {
+//       throw new Error("Pizza name already exists");
+//     }
+
+//     if (ingredients.length === 0) {
+//       throw new Error("Invalid ingredients");
+//     }
+
+//     for (const ingredient of ingredients) {
+//       if (!this.recognizedIngredients.includes(ingredient)) {
+//         throw new Error("Invalid ingredients");
+//       }
+//     }
+
+//     const pizza = new Pizza(name, ingredients);
+//     this.pizzas.push(pizza);
+//     return pizza;
+//   }
+
+//   hasPizza(name) {
+//     return this.pizzas.some(
+//       (pizza) => pizza.name.toLowerCase() === name.toLowerCase().trim()
+//     );
+//   }
+
+//   getPizzaCount() {
+//     return this.pizzas.length;
+//   }
+
+//   addIngredient(ingredient) {
+//     this.recognizedIngredients.push(ingredient);
+//   }
+// }
+
+// module.exports = { basicReduce };
+
+function basicReduce(inputArr) {
+  return inputArr.reduce((acc, cur) => (acc += cur), 0);
+}
+console.log(basicReduce([1, 2, 3, 4, 5]));
+
+module.exports = { basicReduce };
